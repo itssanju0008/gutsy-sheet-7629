4309b3468cbbaae7b9e5d622246b1a425bac7966
sanju682295 <sanju682295@gmail.com>
2022-09-29 14:40:19 +0530

    day-2

--- my-app/src/Components/Main_Navbar/Navbar.jsx
+++ my-app/src/Components/Main_Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 import './Navbar.css'
 import {SearchIcon} from "@chakra-ui/icons"
 import Dropdown from './Dropdown'
-import {HamburgerIcon} from "@chakra-ui/icons"
+import {HamburgerIcon} from "@chakra-ui/icons";
+import { useState } from 'react';
+
+import Items from './Dropdown-items';
+
 const Navbar=()=>{
+    const [state,setState]=useState(false)
     return (
         <div className='container'>
             <div className='menu-icon' >
-                <HamburgerIcon style={{fontSize:"30px"}} />
+                <HamburgerIcon style={{fontSize:"30px",color:"#ff3755"}} onClick={()=>setState(!state)} />
                 </div>
          <div id="nav-div-first" >
             <img id="logo" src="https://cdn2.stylecraze.com/wp-content/themes/buddyboss-child/images/sc-logo.png" alt="web-logo" />
@@ -18,6 +23,7 @@ const Navbar=()=>{
          <div id="list-items" >
           <Dropdown/>
          </div>
+         {state && <Items className="burgerr" />}
         </div>
     )
 }
